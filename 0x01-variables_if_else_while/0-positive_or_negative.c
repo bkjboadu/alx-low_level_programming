fa267e69f412eb335e87aee668e35b5b45cc2466
bkjboadu <brightboadujnr@gmail.com>
2023-02-17 14:11:33 -0800

    l

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,12 +15,15 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive", n);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		printf("%d is negative", n);
-	} else
+	}
+	else
 	{
 		printf("%d is zero", n);
 	}
+	return (0);
 }
 

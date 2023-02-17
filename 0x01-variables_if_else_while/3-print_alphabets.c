8c4f33209a5868bb7ca7e8f997ffeccaf494502a
bkjboadu <brightboadujnr@gmail.com>
2023-02-17 15:29:25 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,23 +7,16 @@
 int main(void)
 {
 	char ch = 'a';
+	char N = 'A';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
+	for (N = 'A'; N <= 'Z'; N++)
+	{
+		putchar(N);
+	}
 	putchar('\n');
 	return (0);
 }
-#include <stdio.h>
-#include <string.h>
-/**
- * main - Print alphabets in lowercase
- * Return: Always 0(successful)
- */
-int main(void)
-{
-        char ch = 'a';
-
-        for (ch = 'a'; ch <= 'z'; ch++)
-

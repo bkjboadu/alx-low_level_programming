5a81d345656d5f46579703aeceacdfc25c868a55
bkjboadu <brightboadujnr@gmail.com>
2023-02-19 12:05:26 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - invent
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n)
+		int m;
+		
+		for (m = '0'; m <= '9'; m++)
+		{
+			if (m == n);
+			continue;
+			else
+			{
+				putchar(m);
+				putchar(',');
+				putchar(' ');
+			}
+			
+		}

e60d4edceeb437656b8fe279ca912038f9622a52
bkjboadu <brightboadujnr@gmail.com>
2023-02-22 02:04:12 -0800

    b;

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digits
+ *
+ * @a: Received argument
+ * Return: k
+ */
+
+int print_last_digit(int a)
+{
+	int k;
+
+	k = a % 10;
+	if (a < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}

277898dada59086b6e317c35e653d7c255ba273d
bkjboadu <brightboadujnr@gmail.com>
2023-02-22 04:09:37 -0800

    k

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,8 +27,8 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d", i);
-                        if (i != 98)
-                                printf(", ");
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -37,8 +37,8 @@ void print_to_98(int n)
 		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d", i);
-                        if (i != 98)
-                                printf(", ");
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}

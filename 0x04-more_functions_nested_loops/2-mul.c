6fe33437acd754160e49cdcfaad78f975a76bdc2
bkjboadu <brightboadujnr@gmail.com>
2023-02-23 14:58:30 -0800

    ./2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int _putchar(char c);
 int _islower(int c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif

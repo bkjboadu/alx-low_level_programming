02c80f6340edf45bf32949992c804633255e76d9
bkjboadu <brightboadujnr@gmail.com>
2023-03-13 11:17:13 -0700

    k

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
-		return (NLL);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
